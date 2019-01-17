18784e57421efeb22048898c9df7ac21da8fddc7
Paul Irish <paulirish@google.com>
2019-01-17 12:01:38 -0800

    types

--- lighthouse-viewer/app/src/lighthouse-report-viewer.js
+++ lighthouse-viewer/app/src/lighthouse-report-viewer.js
@@ -106,7 +106,7 @@ class LighthouseReportViewer {
 
   /**
    * Basic Lighthouse report JSON validation.
-   * @param {LH.ReportResult} reportJson
+   * @param {LH.Result} reportJson
    * @private
    */
   _validateReportJson(reportJson) {
@@ -132,14 +132,15 @@ class LighthouseReportViewer {
   }
 
   /**
-   * @param {LH.ReportResult} json
+   * @param {LH.Result} json
    * @private
    */
   _replaceReportHtml(json) {
     // Allow users to view the runnerResult
-    if (json.lhr) {
-      json = json.lhr;
+    if ('lhr' in json) {
+      json = /** @type {LH.RunnerResult} */ (json).lhr;
     }
+
     this._validateReportJson(json);
 
     if (json.lighthouseVersion.startsWith('2')) {
@@ -207,7 +208,7 @@ class LighthouseReportViewer {
 
   /**
    * Stores v2.x report in IDB, then navigates to legacy viewer in current tab
-   * @param {LH.ReportResult} reportJson
+   * @param {LH.Result} reportJson
    * @private
    */
   _loadInLegacyViewerVersion(reportJson) {
@@ -246,7 +247,7 @@ class LighthouseReportViewer {
 
   /**
    * Saves the current report by creating a gist on GitHub.
-   * @param {LH.ReportResult} reportJson
+   * @param {LH.Result} reportJson
    * @return {Promise<string|void>} id of the created gist.
    * @private
    */
